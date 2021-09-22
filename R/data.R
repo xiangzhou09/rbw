1e46c8bc2f6f9980784240c34b3e3f06dbfb759e
Zhou <xiang_zhou@fas.harvard.edu>
2021-09-22 15:07:08 -0400

    update datasets documentation, add vignette

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 #' A dataset containing 19 variables and 565 unit-week records on the campaign of 113 Democratic candidates
 #' in US Senate and Gubernatorial Elections from 2000 to 2006 (Blackwell 2013).
 #'
-#' @format A data frame with 565 rows and 17 columns: \describe{
+#' @format A data frame with 565 rows and 19 columns: \describe{
 #'   \item{demName}{name of the Democratic candidate}
 #'   \item{d.gone.neg}{whether the candidate went negative in a campaign-week,
 #'    defined as whether more than 10\% of the candidate's political advertising was negative}
@@ -71,33 +71,6 @@
 #'   Political Science. American Journal of Political Science 57(2): 504-619.
 "campaign_wide"
 
-#' Data on Education Attainment and Mental Health in a Sample of US adults.
-#'
-#' A dataset containing 14 variables on education, socioeconomic status,
-#' mental health, gender, race, and family background for 2,835 respondents from
-#' the National Longitudinal Survey of Youth, 1979 (NLSY79)
-#'
-#' @format A data frame with 2,835 rows and 14 columns: \describe{
-#'   \item{id}{respondent id}
-#'   \item{weights}{NLSY79 sampling weight}
-#'   \item{college}{whether the respondent obtained a Bachelor's degree by age 25}
-#'   \item{ses}{socioeconomic status, measured by the percentile rank of the respondent's
-#'   family income at age 40.}
-#'   \item{cesd40}{depression score at age 40, measured by the Center for Epidemiologic Studies
-#'   Depression Scale}
-#'   \item{female}{a dummy variable indicating whether the respondent is female.}
-#'   \item{race}{the race of the respondent, 1: Hispanic; 2: black; 3: other.}
-#'   \item{momedu}{mother's education, measured by highest grade completed}
-#'   \item{parinc}{the percentile rank of parent income in 1979-1983}
-#'   \item{afqt3}{cognitive ability in 1981, measured by the Armed Forces Qualification Test}
-#'   \item{educexp}{educational expectation in 1979}
-#'   \item{cesd92}{depression score in 1992, measured by the Center for Epidemiologic Studies
-#'   Depression Scale}
-#'   \item{prmarr98}{proportion of time married between 1990 and 1998}
-#'   \item{transitions98}{number of family transitions between 1990 and 1998}
-#'   }
-"education"
-
 #' Data on Political Advertisement and Campaign Contributions in US Presidential Elections
 #'
 #' A dataset containing 15 variables on the campaign contributions of 16,265 zip codes to the
@@ -106,20 +79,20 @@
 #'
 #' @format A data frame with 16,265 rows and 15 columns: \describe{
 #'   \item{zip}{zip code}
-#'   \item{TotAds}{the total number of political advertisements aired in the zip code}
 #'   \item{treat}{the Box-Cox transformed TotAds (Fong, Hazlett, and Imai 2018)}
+#'   \item{TotAds}{the total number of political advertisements aired in the zip code}
 #'   \item{TotalPop}{population size}
-#'   \item{log_TotalPop}{log population}
-#'   \item{Inc}{median household income}
-#'   \item{log_Inc}{log median income}
 #'   \item{PercentOver65}{percent of the population over 65}
+#'   \item{Inc}{median household income}
 #'   \item{PercentHispanic}{percent Hispanic}
 #'   \item{PercentBlack}{percent black}
-#'   \item{per_collegegrads}{percent college graduates}
 #'   \item{density}{population density (people per sq mile)}
+#'   \item{per_collegegrads}{percent college graduates}
 #'   \item{CanCommute}{a dummy variable indicating whether it is possible to commute to the zip code from a competitive state}
-#'   \item{Cont}{campaign contributions (in thousands of dollars)}
 #'   \item{StFIPS}{state FIPS code}
+#'   \item{Cont}{campaign contributions (in thousands of dollars)}
+#'   \item{log_TotalPop}{log population}
+#'   \item{log_Inc}{log median income}
 #'   }
 #' @references Fong, Christian, Chad Hazlett, and Kosuke Imai. 2018. Covariate Balancing Propensity Score for a Continuous
 #'   Treatment: Application to The Efficacy of Political Advertisements. The Annals of Applied Statistics 12(1):156-77.
@@ -138,10 +111,6 @@
 #'
 #' @format A data frame with 1,273 rows and 17 columns: \describe{
 #'   \item{threatc}{number of adverse events respondents considered probable if the US did not engage in war}
-#'   \item{cost}{number of negative consequences anticipated if the US engaged in war}
-#'   \item{successc}{whether the respondent thought the operation would succeed. 0: less than 50-50 chance of working even in the short run; 1: efficacious only in the short run; 2: successful both in the short and long run}
-#'   \item{democ}{a dummy variable indicating whether the country was a democracy}
-#'   \item{immoral}{a dummy variable indicating whether respondents thought it would be morally wrong to strike the country}
 #'   \item{ally}{a dummy variable indicating whether the country had signed a military alliance with the US}
 #'   \item{trade}{a dummy variable indicating whether the country had high levels of trade with the US}
 #'   \item{h1}{an index measuring respondent's attitude toward militarism}
@@ -153,7 +122,11 @@
 #'   \item{white}{a dummy variable indicating whether the respondent is white}
 #'   \item{age}{respondent's age}
 #'   \item{ed4}{respondent's education with categories ranging from high school or less to postgraduate degree}
+#'   \item{democ}{a dummy variable indicating whether the country was a democracy}
 #'   \item{strike}{a measure of support for war on a five-point scale}
+#'   \item{cost}{number of negative consequences anticipated if the US engaged in war}
+#'   \item{successc}{whether the respondent thought the operation would succeed. 0: less than 50-50 chance of working even in the short run; 1: efficacious only in the short run; 2: successful both in the short and long run}
+#'   \item{immoral}{a dummy variable indicating whether respondents thought it would be morally wrong to strike the country}
 #'   }
 #' @references Tomz, Michael R., and Jessica L. P. Weeks. 2013. Public Opinion and the Democratic Peace.
 #'   The American Political Science Review 107(4):849-65.
